e39e5ed0081ae9e03475545e31df337befec38b7
FrankyCunza <franky.cunza@gmail.com>
2021-02-21 11:36:18 -0500

    last changes

--- src/app/solution/solution.component.ts
+++ src/app/solution/solution.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solution.component.css']
 })
 export class SolutionComponent implements OnInit {
-
+  public images = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.images = [
+      {
+        image: 'gallery1',
+        description: 'Imagen 1'
+      },
+      {
+        image: 'gallery2',
+        description: 'Imagen 2'
+      },
+      {
+        image: 'gallery3',
+        description: 'Imagen 3'
+      },
+      {
+        image: 'gallery4',
+        description: 'Imagen 4'
+      },
+      {
+        image: 'gallery5',
+        description: 'Imagen 5'
+      }
+    ]
+  }
+
+  sliderSolutions(index) {
+    const thumbs = document.getElementsByClassName("solution__thumbs__item");
+    for (var i=0; i<thumbs.length;i++) {
+      if(i == index) {
+        thumbs[i].classList.add("solution__thumbs__item--active")
+      } else {
+        thumbs[i].classList.remove("solution__thumbs__item--active")
+      }
+    }
+
+    document.getElementById(`solution__slider__content`).style.transform = `translateX(-${index}00%)`;
   }
 
 }
