719650b21e62a67d954a922e6cea19cfa86d3011
FrankyCunza <franky.cunza@gmail.com>
2021-02-21 17:59:55 -0500

    last changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -15,21 +15,22 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
   
-  private lheaders = new HttpHeaders().set('authorization', 'Basic a0ca60628a35ebdcf4743210da53ed0a-us17');
-  // private lheaders = new HttpHeaders().set('Access-Control-Allow-Origin', '*');
   // private lheaders = new HttpHeaders();
+  // private lheaders = new HttpHeaders().set('authorization', 'Basic a0ca60628a35ebdcf4743210da53ed0a-us17');
   // https://us17.api.mailchimp.com/2.0/lists/subscribe.json?apikey=a0ca60628a35ebdcf4743210da53ed0a-us17&id=33d7e5750f&email[email]=conrad7@gmail.com&merge_vars[MMERGE3]=65555555&merge_vars[FNAME]=John&merge_vars[LNAME]=Doe&double_optin=false&send_welcome=false
   submit(e) {
     e.preventDefault();
+    // this.lheaders.append('Access-Control-Allow-Origin', '*');
     // this.lheaders.append('Content-Type', 'application/json');
-    // this.lheaders.append("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
+    // this.lheaders.append("Access-Control-Allow-Methods", "POST")
     // this.lheaders.append("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
     // this.lheaders.append('authorization', 'Basic a0ca60628a35ebdcf4743210da53ed0a-us17');
-    console.log(this.lheaders)
-    console.log("cargando")
-    // lheaders.set('Access-Control-Allow-Origin', '*',)
-    // lheaders.set('authorization', 'Basic a0ca60628a35ebdcf4743210da53ed0a-us17')
-    // lheaders.set('Content-Type', 'application/json',)
+    const mheaders = {
+      'authorization': 'Basic a0ca60628a35ebdcf4743210da53ed0a-us17'
+    };
+
+    console.log(mheaders)
+    console.log("cargando", "version 1")
     const data = {
       "email_address":"laptop@gmail.com",
       "email_type":"text",
@@ -51,7 +52,7 @@ export class ContactComponent implements OnInit {
       "tags":[]
     }
 
-    this.http.post('https://us17.api.mailchimp.com/3.0/lists/33d7e5750f/members?skip_merge_validation=true', JSON.stringify(data), {headers: this.lheaders}).subscribe(res => {
+    this.http.post('https://us17.api.mailchimp.com/3.0/lists/33d7e5750f/members?skip_merge_validation=true?apikey=a0ca60628a35ebdcf4743210da53ed0a-us17', JSON.stringify(data), {headers: mheaders}).subscribe(res => {
       console.log(res);
     })
   }
