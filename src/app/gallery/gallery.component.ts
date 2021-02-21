2042982ecbb559db227281dda3e4ae0c59a60942
FrankyCunza <franky.cunza@gmail.com>
2021-02-21 08:43:43 -0500

    last changes

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
-
+  public images =[];
   constructor() { }
 
   ngOnInit(): void {
+    this.images = [
+      {
+        image: 'gallery1',
+        description: 'Imagen 1'
+      },
+      {
+        image: 'gallery2',
+        description: 'Imagen 2'
+      },
+      {
+        image: 'gallery3',
+        description: 'Imagen 3'
+      },
+      {
+        image: 'gallery4',
+        description: 'Imagen 4'
+      },
+      {
+        image: 'gallery5',
+        description: 'Imagen 5'
+      }
+    ]
   }
 
 }
